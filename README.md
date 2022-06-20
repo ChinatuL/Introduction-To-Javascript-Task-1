11182b74a9aeda9a07271804ab9fcd9c3261b374
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-06-20 18:20:02 +0100

    update readme and add javascript code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Introduction-To-Javascript-Task-1
+
+This repo contains github code that prints my name, height and country. A task for the I4GZuri Training 2022.
---
+++ script.js
@@ -0,0 +1,3 @@
+let personalInfo =
+    "My name is Chinatu Eke. I am 5feet 4inches tall and I am a Nigerian";
+document.getElementById("personal-info").textContent = personalInfo;
